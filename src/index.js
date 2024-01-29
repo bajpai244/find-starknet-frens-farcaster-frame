917fa8a9adc9f2e86ab65cd48b2c769d8d87411f
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-01-29 21:40:05 +0530

    feat: add support for automatic download

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+const crypto = require('crypto');
+const fs = require('fs');
 const puppeteer =require('puppeteer');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const main = async () => {
     const browser = await puppeteer.launch({ headless: true, defaultViewport: {
@@ -6,12 +11,27 @@ const main = async () => {
         height: 933,
         isLandscape: true
     } });
+
+    const frenList = await fetchFrenList(browser);
+    fs.writeFileSync('./data/frenList.json', JSON.stringify(frenList));
+
+    for (let i = 0; i < frenList.length; i++) {
+        const fren = frenList[i];
+        let frenFarCasterProfileUrl = fren.frenFarCasterProfileUrl;
     const page = await browser.newPage();
-    await page.goto("https://warpcast.com/0d1n-fr33",   { waitUntil: 'networkidle2' });
+    await page.goto(frenFarCasterProfileUrl,   { waitUntil: 'networkidle2' });
     await sleep(5000);
 
-    await page.screenshot({ path: "./data/screenshot.png", clip: clipRegion });
+    await page.screenshot({ path: `./data/${generateSHA256Hash(frenFarCasterProfileUrl)}.png`, clip: clipRegion });
     await page.close();
+    }
+
+}
+
+function generateSHA256Hash(inputString) {
+    const hash = crypto.createHash('sha256');
+    hash.update(inputString);
+    return hash.digest('hex');
 }
 
 // Example usage:
@@ -26,5 +46,33 @@ const clipRegion = {
   const sleep = async (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
+
+  const fetchFrenList = async (browser) => {
+    const page = await browser.newPage();
+    await page.goto("https://github.com/keep-starknet-strange/starknet-warpcast/blob/main/builder_follow_builder.md",   { waitUntil: 'domcontentloaded' });
+    await sleep(2000);
+
+   let result = await page.evaluate(() => {
+        const arr = [];
+        let table = document.getElementsByTagName('table')[0];
+        let tableBody = table.children[1];
+
+        for (let i =0 ; i < tableBody.children.length; i++) {
+            let row = tableBody.children[i];
+            let frenName = row.children[0].innerText;
+            let frenFarCasterProfileUrl =  row.children[1].children[0].href;
+
+            arr.push({frenName, frenFarCasterProfileUrl});
+        }
+
+        return arr;
+    });
+
+    await page.close();
+
+    return result;
+  }
+
+
 
 main();
