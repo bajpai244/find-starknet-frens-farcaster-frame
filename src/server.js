d98bf0c09f304e2e9b7fcd3333c4f1ffb42fbd39
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-01-31 20:01:26 +0530

    feat: fix reponse

--- src/server.js
+++ src/server.js
@@ -64,6 +64,11 @@ app.post('/next_profile', (req, res) => {
       property="fc:frame:post_url"
       content="https:://kakachain.xyz:3000/next_profile"
     />
+    <meta property="fc:frame:button:1" content="Find Next Fren :)" />
+    <meta
+      property="fc:frame:post_url"
+      content="https://kakachain.xyz:3000/next_profile"
+    />
   </head>
   <body>
     <h1>Starknet follow fellow builders</h1>
